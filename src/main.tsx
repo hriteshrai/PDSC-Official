b64cff2e0ffd1cce3feb176e96c83800022153f0
Darshan Poudel <pranishpoudel10@gmail.com>
2024-05-14 22:26:51 +0545

    /power-series end point added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import RedirectToPowerSeries from './utils/RedirectToPowerSeries.tsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/power-series",
+    element: <RedirectToPowerSeries />,
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ src/utils/RedirectToPowerSeries.tsx
@@ -0,0 +1,8 @@
+import { useEffect } from 'react';
+
+export default function RedirectToPowerSeries() {
+  useEffect(() => {
+    window.location.href = "https://meet.google.com/ywa-muhp-bsi"
+  },[])
+  return null;
+}
